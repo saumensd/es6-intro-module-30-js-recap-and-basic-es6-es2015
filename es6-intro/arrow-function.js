18542adbf3a5a8485fd1b72287ebb7e5145d1927
saumensd <saumensutradhar@gmail.com>
2021-08-23 20:36:40 +0600

    summary commit

--- es6-intro/arrow-function.js
+++ es6-intro/arrow-function.js
@@ -10,11 +10,14 @@ const add2 = function add2(num1, num2){
 const add3 = function (num1,num2){
     return num1 + num2;
 }
+// arrow function
+const add4 = (num1, num2) => num1 +num2;
 
 const sum1 = add(15, 17);
 const sum2 = add2(15, 17);
 const sum3 = add3(15, 17);
-console.log(sum1, sum2, sum3);
+const sum4 = add4(15, 17);
+console.log(sum1, sum2, sum3, sum4);
 // document.getElementById('my-btn').onclick = function handleEvent(){
 
 // }
---
+++ es6-intro/summary.js
@@ -0,0 +1,25 @@
+let priyoPerson = 'Koolsum Begum';
+priyoPerson = 'Momotaj Begum';
+const hubby = 'kasem';
+
+// defult parameter
+function getName(first, lat='chowdhury'){
+return first + ' ' + last;
+}
+//template string
+const myPeople = `My lovely peson is are ${hubby} and his fullName is ${getName('Akbar')}.My name is ${priyoPerson}.`
+
+console.log(myPeople);
+
+//arrow function 
+const getName2 = (first, last) => first + ' ' + last;
+const fiveTimes = x => x*5;
+const bigArrowFunc = (x, y, z) => {
+const firstPart = x + y;
+const secondPart  = y * z;
+const thirdPart = x / z;
+const result = (firstPart + secondPart) * thirdPart;
+return result;
+}
+const numbers = [2, 4, 67, 54];
+const min = Math.min(...numbers)
